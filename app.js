c54cc30b527b1b58231fbb91edc7cab31601da67
Lindgard <a.b.lindgard@gmail.com>
2022-10-10 16:20:02 +0200

    first commit for backup and version-control

---
+++ app.js
@@ -0,0 +1,20 @@
+const apiUsers = [];
+
+async function getUsers() {
+  const response = await fetch("https://randomuser.me/api/?results=50");
+  const jsonData = await response.json();
+  return jsonData.results;
+}
+
+// async funksjon for å legge informasjon fra fetch inn i array
+//apiUsers = getUsers();
+
+console.log(apiUsers);
+
+// funksjon for å printe til DOM/browser med callback til en print-funksjon
+// som henter informasjon fra enten input eller API, så parametere til dette
+
+// CRUD!!!!!!!!!!!!!!!!
+// array-methods, math.random(), addEventListener +/ Onclick, prompt + alert, loop
+// createElement, classList.add og remove, søkefunksjonalitet, filtrering, kart for
+// lokasjon fra array-data basert på coordinater i objektets informasjon
